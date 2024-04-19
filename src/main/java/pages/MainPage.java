ab89470ab510e0ab5ce30d3ab23ea10d36a4163b
Konstantin Demidov <88594050+KoldKilla@users.noreply.github.com>
2024-04-19 17:18:43 +0300

    Update MainPage.java

--- src/main/java/pages/MainPage.java
+++ src/main/java/pages/MainPage.java
@@ -2,12 +2,20 @@ package pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
-;
+;import java.time.Duration;
 
 
 public class MainPage {
-    private static WebDriver driver;
+
+    protected static WebDriver driver;
+
+    public static void setDriver(WebDriver driver) {
+        MainPage.driver = driver;
+    }
 
     public MainPage(WebDriver driver) {
         this.driver = driver;
@@ -25,6 +33,17 @@ public class MainPage {
     public MainPage clickDropDownMenuItemsText(){
         driver.findElement(dropDownMenuItemsText).click();
         return this;
+    }
+
+    public static boolean isElementVisibleAndClickable(WebElement webElement) {
+        try {
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+            wait.until(ExpectedConditions.visibilityOf(webElement));
+            wait.until(ExpectedConditions.elementToBeClickable(webElement));
+            return true;
+        } catch (org.openqa.selenium.NoSuchElementException e) {
+            return false;
+        }
     }
 }
 
